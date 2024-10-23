7c6027e53a381642416742442beaab6a12fe4374
Darcey Mckelvey <darceymckelvey@gmail.com>
2024-10-23 20:49:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,41 @@ function getUser(id, callback) {
                 githubUserName: "darceymckelvey"
             };
             callback(user);
-        }
-    );
+        }, 3000);
 }
 
 getUser(1, (user) => {
     console.log(user);
 });
+
+// promise
+// a promise is an object that represents the eventual completion or failure of a async operation
+
+// pending => fulfiled, or rejected
+function getUserPromis(id) {
+    return new Promise((resolve, reject)=>{
+            setTimeout(() => {
+            console.log(`Reading user ID: ${id} from database...`);
+            let user = {
+                id: id,
+                githubUserName: "chucknorris"
+            };
+
+            if (user) {
+                resolve(user);
+            } else {
+                reject(new Error('User not found.'));
+            }
+        }, 3000);
+    })
+}
+
+getUserPromis(2).then(
+    (user) => {
+        console.log(user);
+    }
+).catch(
+    (error) => {
+    console.log(error);
+    }
+);
